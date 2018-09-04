0d8072bc484735ab1abf918bdb9dee04d4bcfca5
Ruud van Buul <ruudvanbuul@gmail.com>
2018-09-03 21:58:59 -0600

    Add prototype documentation

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-08-24 01:40:15.
+    * Generated on 2018-09-04 03:57:27.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.34 on 2018-08-24 01:40:11.
+ * Generated for Laravel 5.6.37 on 2018-09-04 03:57:26.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
